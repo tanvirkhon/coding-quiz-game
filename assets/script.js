4c0ac174c994f8353378eec8bc4b53993809df14
Hoard Coin <119143763+tanvirkhon@users.noreply.github.com>
2023-01-09 23:26:08 -0600

    Added a save score function at end of quiz

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,12 @@
 var mainScreen = document.getElementById("main-screen");
 var questionScreen = document.getElementById("question-screen");
 var playerTime = document.getElementById("player-time");
+var playerNameSection = document.getElementById("player-name-section");
 var playerName = document.getElementById("player-name");
 var saveButton = document.getElementById("save");
+var restartButton = document.getElementById("restart");
 var questionIndex;
-var timeLeft = 60;
+var timeLeft = 25;
 var playerScore = 0;
 
 var questions = [
@@ -30,6 +32,20 @@ var questions = [
     choice3: "Splice",
     choice4: "Slice",
   },
+  {
+    question: "JavaScript entities start with _______ and end with _________.",
+    choice1: "Semicolon, colon",
+    choice2: "Semicolon, Ampersand",
+    choice3: "Ampersand, semicolon",
+    choice4: "Ampersand, colon",
+  },
+  {
+    question: "JavaScript is interpreted by _________",
+    choice1: "Server",
+    choice2: "Object",
+    choice3: "Client",
+    choice4: "None of the above",
+  },
 ];
 
 // Function to start quiz
@@ -50,8 +66,10 @@ function countdown() {
     if (timeLeft === 0) {
       clearInterval(timeInterval);
       questionScreen.classList.add("hide");
-      playerName.classList.remove("hide");
-      console.log("time up");
+      playerNameSection.classList.remove("hide");
+      document.getElementById("hs-player-name").textContent =
+        "Your Score is: " + playerScore + " out of 5";
+      renderLastScore();
     }
   }, 1000);
 }
@@ -73,54 +91,52 @@ function displayQuestion() {
 // Function to check if answer is clicked and move to the next question
 function checkAnswer(event) {
   console.log(event);
-  if (questionIndex === 2) {
+  if (questionIndex === 4) {
     questionIndex = 0;
     questionScreen.classList.add("hide");
-    playerName.classList.remove("hide");
+    playerNameSection.classList.remove("hide");
+    document.getElementById("hs-player-name").textContent =
+      "Your Score is: " + playerScore + " out of 5";
+    renderLastScore();
   }
 
   if (event.target.matches("button#choice-three")) {
     console.log("A button was clicked");
     console.log(event.target.textContent);
-    playerScore = playerScore + 5;
-    console.log("Player score is: " + playerScore);
+    playerScore = playerScore + 1;
+    console.log("Player score is: " + playerScore + " out of 5");
     questionIndex++;
     displayQuestion();
   } else {
-    timeLeft = timeLeft - 10;
+    timeLeft = timeLeft - 5;
     questionIndex++;
     displayQuestion();
   }
 }
 
 // Function to save player name and score
-function saveLastScore() {
-  // Save related form data as an object
-  var playerHighScore = {
-    student: playerName.input,
-  };
-  // Use .setItem() to store object in storage and JSON.stringify to convert it as a string
-  localStorage.setItem("playerHighScore", JSON.stringify(playerHighScore));
-  return;
-}
-
-function renderLastScore() {
-  // Use JSON.parse() to convert text to JavaScript object
-  var lastScore = JSON.parse(localStorage.getItem("playerHighScore"));
-  // Check if data is returned, if not exit out of the function
-  if (lastScore !== null) {
-    document.getElementById("hs-player-name").innerHTML = lastScore.student;
-  } else {
-    return;
-  }
-}
-
 saveButton.addEventListener("click", function (event) {
   event.preventDefault();
-  saveLastScore();
-  renderLastScore();
+  var playerHighScore = {
+    player: playerName.value,
+    score: playerScore,
+  };
+  localStorage.setItem("playerHighScore", JSON.stringify(playerHighScore));
+  return;
 });
 
+function renderLastScore() {
+  var lastScore = JSON.parse(localStorage.getItem("playerHighScore"));
+  document.getElementById("high-scores").innerHTML =
+    lastScore.player + " your last score was: " + lastScore.score + " out of 5";
+}
+
+// Restart Button Function
+restartButton.addEventListener("click", function () {
+  window.location.reload();
+});
+
+// Start Quiz on Click
 document.getElementById("start-button").addEventListener("click", startQuiz);
 
 questionScreen.addEventListener("click", checkAnswer);
